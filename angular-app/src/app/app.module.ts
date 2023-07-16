aed9aa8eaa232dc6a076caee928e9f935ce34e6d
yyuuiikk <yutaikawa0315@gmail.com>
2023-07-16 13:48:58 +0900

    add 5

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
